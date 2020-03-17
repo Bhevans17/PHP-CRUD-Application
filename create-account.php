47c8291e9a25884da0c685c72d702bca9794db19
Brenden <bhevans17@gmail.com>
2020-03-16 18:06:18 -0700

    HF

--- create-account.php
+++ create-account.php
@@ -1,12 +1,12 @@
 <?php session_start(); ?>
-<?php include "config.php" ?>
+<?php include "./config/config.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href="./css/style.css" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Create Account</title>
 </head>
@@ -23,7 +23,7 @@
 
                 $username = $_POST['username'];
                 $email = $_POST['email'];
-                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $password = $_POST['password'];
 
                     $sql = "INSERT INTO user (username, email, `password`)
                     VALUES ('$username', '$email', '$password')";
@@ -75,6 +75,7 @@
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="./js/index.js"></script>
         <script src="./js/create-account.js"></script>
 </body>
 
---
+++ test2.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Test2 {
+    private $prop_one = "Prop One";
+
+
+    // Methods
+    function set_name($name) {
+        $this->name = $name;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+}
